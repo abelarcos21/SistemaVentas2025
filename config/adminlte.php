f3d31d9fa41c39c3685f3e7ec9092946ff9b6aa8
abel302025@hotmail.com <abel302025@hotmail.com>
2025-05-02 23:07:17 -0600

    agregando estilos al login

--- config/adminlte.php
+++ config/adminlte.php
@@ -172,10 +172,10 @@ return [
     */
 
     'classes_auth_card' => 'card-outline card-primary',
-    'classes_auth_header' => '',
-    'classes_auth_body' => '',
+    'classes_auth_header' => 'bg-secondary',
+    'classes_auth_body' => 'bg-secondary',
     'classes_auth_footer' => '',
-    'classes_auth_icon' => '',
+    'classes_auth_icon' => 'text-white',
     'classes_auth_btn' => 'btn-flat btn-primary',
 
     /*
