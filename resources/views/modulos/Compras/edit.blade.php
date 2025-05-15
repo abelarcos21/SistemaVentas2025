14cae701c99eb933739c6abf7b78fc6109940912
abelarcos <abel302025@hotmail.com>
2025-05-15 11:16:24 -0600

    agregando modificaciones en las vista de index de botones de exportacion en detalle de ventas,ventas y compras

--- resources/views/modulos/Compras/edit.blade.php
+++ resources/views/modulos/Compras/edit.blade.php
@@ -8,7 +8,7 @@
         <div class="container-fluid">
           <div class="row mb-2">
             <div class="col-sm-6">
-              <h1> <i class="fas fa-shopping-cart"></i> Compras | Editar Una Compra</h1>
+              <h1> <i class="fas fa-edit"></i>  Compras | Editar Una Compra</h1>
             </div>
             <div class="col-sm-6">
               <ol class="breadcrumb float-sm-right">
@@ -27,9 +27,9 @@
       <div class="container-fluid">
         <div class="row">
             <div class="col-12">
-                <div class="card card-outline card-info">
+                <div class="card card-outline card-warning">
                     <div class="card-header bg-secondary">
-                        <h3 class="card-title">Edicion de : {{ $compra->nombre_producto }}</h3>
+                        <h3 class="card-title">Edicion de : <span class="badge bg-warning text-dark">{{ $compra->nombre_producto }}</span></h3>
                     </div>
                     <!-- /.card-header -->
                     <div class="card-body bg-secondary">
@@ -78,8 +78,8 @@
                                 <!-- /.card-body -->
 
                                 <div class="card-footer bg-secondary">
-                                    <button type="submit" class="btn btn-info">
-                                        <i class="fas fa-shopping-cart"></i> Actualizar
+                                    <button type="submit" class="btn btn-warning">
+                                         <i class="fas fa-save"></i> Actualizar
                                     </button>
                                     <a href="{{ route('compra.index')}}" class="btn btn-secondary float-right">
                                         <i class="fas fa-times"></i> Cancelar
