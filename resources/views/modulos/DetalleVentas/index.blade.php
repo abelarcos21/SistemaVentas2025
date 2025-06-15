86d8373365a71f50c1db1913877ec979c85be29b
abel302025@hotmail.com <abel302025@hotmail.com>
2025-06-15 17:57:56 -0600

    agregando titulos al modulo de ventas

--- resources/views/modulos/DetalleVentas/index.blade.php
+++ resources/views/modulos/DetalleVentas/index.blade.php
@@ -8,7 +8,7 @@
         <div class="container-fluid">
           <div class="row mb-2">
             <div class="col-sm-6">
-              <h1><i class="fas fa-history"></i> Historial De Ventas Realizadas</h1>
+              <h1><i class="fas fa-history"></i> Ventas | Historial De Ventas Realizadas</h1>
             </div>
             <div class="col-sm-6">
               <ol class="breadcrumb float-sm-right">
