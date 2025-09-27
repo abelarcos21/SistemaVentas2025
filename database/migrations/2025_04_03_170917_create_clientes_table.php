30f08128358744675f6bf1c0a001268296127ac6
abelarcos <abel302025@hotmail.com>
2025-09-27 12:50:39 -0600

    =agregando cambios en el clientes de la tabla quitando campos de cfdi y eliminando la migracion de catalogos y impuestos

--- database/migrations/2025_04_03_170917_create_clientes_table.php
+++ database/migrations/2025_04_03_170917_create_clientes_table.php
@@ -13,16 +13,6 @@ return new class extends Migration
     {
         Schema::create('clientes', function (Blueprint $table) {
             $table->id();
-            $table->string('razon_social')->nullable();//para que pase el seeder se puso nullable
-            $table->string('nombre_comercial')->nullable();
-            $table->string('calle')->nullable();//para que pase el seeder se puso nullable
-            $table->string('numero_exterior')->nullable();//para que pase el seeder se puso nullable
-            $table->string('numero_interior')->nullable();
-            $table->string('codigo_postal')->nullable();//para que pase el seeder se puso nullable;
-            $table->string('estado')->nullable();//para que pase el seeder se puso nullable;
-            $table->string('municipio')->nullable();//para que pase el seeder se puso nullable;
-            $table->string('regimen_fiscal',3)->nullable();// clave SAT//para que pase el seeder se puso nullable;
-            $table->string('uso_cfdi', 5)->nullable(); // clave SAT para que pase el seeder se puso nullable
             $table->string('nombre');
             $table->string('apellido');
             $table->string('rfc',13);
